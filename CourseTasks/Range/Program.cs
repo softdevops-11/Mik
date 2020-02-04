42288b6567e576ae74af152b317c3e17a2479e09
softdevops-11 <mikhailsaraev85@gmail.com>
2020-02-04 21:25:14 +0700

    Создан проект Range(complex)

---
+++ CourseTasks/Range/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace RangeMain
+{
+    internal class RangeMain
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Введите концы диапазона: ");
+            double from = Convert.ToDouble(Console.ReadLine());
+            double to = Convert.ToDouble(Console.ReadLine());
+            Range interval = new Range(from, to);
+
+            double intervalLength = interval.GetLength();
+            Console.WriteLine("Длина интервала равна {0}. ", intervalLength);
+            Console.WriteLine("Введите число:");
+
+            double number = Convert.ToDouble(Console.ReadLine());
+
+            if (interval.IsInside(number))
+            {
+                Console.WriteLine("Число лежит в интервале. ");
+            }
+            else
+            {
+                Console.WriteLine("Число не лежит в интервале. ");
+            }
+
+            Console.WriteLine("Введите концы первого диапазона: ");
+            double fromOne = Convert.ToDouble(Console.ReadLine());
+            double toOne = Convert.ToDouble(Console.ReadLine());
+            Range intervalOne = new Range(fromOne, toOne);
+
+            Console.WriteLine("Введите концы второго диапазона: ");
+            double fromTwo = Convert.ToDouble(Console.ReadLine());
+            double toTwo = Convert.ToDouble(Console.ReadLine());
+            Range intervalTwo = new Range(fromTwo, toTwo);
+
+            Range intervalIntersection = Range.RangesIntersection(intervalOne, intervalTwo);
+
+            if (intervalIntersection != null)
+            {
+                Console.WriteLine("Интервал пересечения [{0},{1}]. ", intervalIntersection.From, intervalIntersection.To);
+            }
+            else
+            {
+                Console.WriteLine("Интервал пересечения null.");
+            }
+
+            Range[] intervalUnion = Range.RangesUnion(intervalOne, intervalTwo);
+            Console.Write("Объединение интервалов дает интервал: ");
+
+            for (int i = 0; i < 2; i++)
+            {
+                if (intervalUnion[i] != null)
+                {
+                    Console.Write(" [{0},{1}] ", intervalUnion[i].From, intervalUnion[i].To);
+                }
+            }
+
+            Console.WriteLine();
+            Range[] intervalDifference = Range.RangesDifference(intervalOne, intervalTwo);
+            Console.Write("Разность интервалов дает интервал: ");
+
+            for (int i = 0; i < 2; i++)
+            {
+                if (intervalDifference[i] != null)
+                {
+                    Console.Write(" [{0},{1}] ", intervalDifference[i].From, intervalDifference[i].To);
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CourseTasks/Range/Range.cs
@@ -0,0 +1,143 @@
+﻿namespace RangeMain
+{
+    internal class Range
+    {
+        public double From { get; private set; }
+
+        public double To { get; private set; }
+
+        public Range(double from, double to)
+        {
+            From = from;
+            To = to;
+        }
+
+        public double GetLength()
+        {
+            return To - From;
+        }
+
+        public bool IsInside(double number)
+        {
+            return number >= From && number <= To;
+        }
+
+        public static Range RangesIntersection(Range intervalOne, Range intervalTwo)
+        {
+            double intersectionFrom = 0;
+            double intersectionTo = 0;
+
+            if (intervalTwo.To >= intervalOne.From && intervalTwo.From <= intervalOne.To)
+            {
+                if (intervalOne.From >= intervalTwo.From)
+                {
+                    intersectionFrom = intervalOne.From;
+                }
+                else
+                {
+                    intersectionFrom = intervalTwo.From;
+                }
+
+                if (intervalOne.To <= intervalTwo.To)
+                {
+                    intersectionTo = intervalOne.To;
+                }
+                else
+                {
+                    intersectionTo = intervalTwo.To;
+                }
+
+                return new Range(intersectionFrom, intersectionTo);
+            }
+
+            return null;
+        }
+
+        public static Range[] RangesUnion(Range intervalOne, Range intervalTwo)
+        {
+            double[] unionFrom = new double[2];
+            double[] unionTo = new double[2];
+            Range[] rangesUnion = new Range[2];
+
+            if (intervalTwo.To >= intervalOne.From && intervalTwo.From <= intervalOne.To)
+            {
+                if (intervalOne.From >= intervalTwo.From)
+                {
+                    unionFrom[0] = intervalTwo.From;
+                }
+                else
+                {
+                    unionFrom[0] = intervalOne.From;
+                }
+
+                if (intervalOne.To <= intervalTwo.To)
+                {
+                    unionTo[0] = intervalTwo.To;
+                }
+                else
+                {
+                    unionTo[0] = intervalOne.To;
+                }
+
+                rangesUnion[0] = new Range(unionFrom[0], unionTo[0]);
+
+                return rangesUnion;
+            }
+            unionFrom[0] = intervalOne.From;
+            unionTo[0] = intervalOne.To;
+            unionFrom[1] = intervalTwo.From;
+            unionTo[1] = intervalTwo.To;
+
+            rangesUnion[0] = new Range(unionFrom[0], unionTo[0]);
+            rangesUnion[1] = new Range(unionFrom[1], unionTo[1]);
+
+            return rangesUnion;
+        }
+
+        public static Range[] RangesDifference(Range intervalOne, Range intervalTwo)
+        {
+            double[] differenceFrom = new double[2];
+            double[] differenceTo = new double[2];
+
+            Range[] rangesDifference = new Range[2];
+
+            if (intervalTwo.To >= intervalOne.From && intervalTwo.From <= intervalOne.To)
+            {
+                if (intervalTwo.From > intervalOne.From && intervalTwo.To < intervalOne.To)
+                {
+                    differenceFrom[0] = intervalOne.From;
+                    differenceTo[0] = intervalTwo.From;
+                    differenceFrom[1] = intervalTwo.To;
+                    differenceTo[1] = intervalOne.To;
+
+                    rangesDifference[0] = new Range(differenceFrom[0], differenceTo[0]);
+                    rangesDifference[1] = new Range(differenceFrom[1], differenceTo[1]);
+                }
+                else if (intervalTwo.From > intervalOne.From || intervalTwo.To < intervalOne.To)
+                {
+                    if (intervalTwo.From > intervalOne.From)
+                    {
+                        differenceFrom[0] = intervalOne.From;
+                        differenceTo[0] = intervalTwo.From;
+                        rangesDifference[0] = new Range(differenceFrom[0], differenceTo[0]);
+                    }
+                    else
+                    {
+                        differenceFrom[0] = intervalTwo.To;
+                        differenceTo[0] = intervalOne.To;
+                        rangesDifference[0] = new Range(differenceFrom[0], differenceTo[0]);
+                    }
+                }
+            }
+            else if (!(intervalTwo.To >= intervalOne.From && intervalTwo.From <= intervalOne.To))
+            {
+                differenceFrom[0] = intervalOne.From;
+                differenceTo[0] = intervalOne.To;
+                rangesDifference[0] = new Range(differenceFrom[0], differenceTo[0]);
+            }
+
+            return rangesDifference;
+        }
+
+    }
+}
