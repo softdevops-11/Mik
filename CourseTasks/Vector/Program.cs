d4d6f9b0d70cd07037fcc906f89759196fcd97fe
softdevops-11 <mikhailsaraev85@gmail.com>
2020-02-10 21:18:04 +0700

    Создан проект Vector

---
+++ CourseTasks/Vector/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Vector
+{
+    class ProgramVector
+    {
+        static void Main()
+        {
+            double[] a = { 1.1, 2.1, 4, 7.5 };
+            Vector vector1 = new Vector(a);
+
+            double[] b = { 3, 4, 5, 6, 7 };
+            Vector vector2 = new Vector(b);
+
+            double[] c = { 1, 1, 1 };
+            Vector vector3 = new Vector(c);
+
+            Console.WriteLine(new Vector(6));
+            Console.WriteLine(new Vector(vector2));
+            Console.WriteLine(new Vector(a));
+            Console.WriteLine(vector1);
+            Console.WriteLine(new Vector(6, b));
+
+            Console.WriteLine("Размерность вектора vector2 {0}", vector2.GetSize());
+
+            vector1.SetVectorValue(1, 10);
+            Console.WriteLine("Значение вектора на 2 позиции {0}", vector1.GetVectorValue(1));
+
+            vector2.AddVector(vector1);
+            Console.WriteLine("Суммирование к вектору vector2 вектора vector1: {0}", vector2);
+
+            vector2.SubtractVector(vector1);
+            Console.WriteLine("Вычитание от вектора vector2 вектора vector1: {0}", vector2);
+
+            vector2.MultiplicateVector(3);
+            Console.WriteLine("Умножение вектора vector2 на скаляр: {0}", vector2);
+
+            vector2.ReverseVector();
+            Console.WriteLine("Обращение вектора vector2: {0}", vector2);
+            Console.WriteLine("Длина вектора vector2: {0}", vector2.GetLengthVector());
+            Console.WriteLine("Сравнение на равенство векторов vector1 и vector2{0}", vector1.Equals(vector2));
+            Console.WriteLine(vector1);
+            Console.WriteLine(vector2);
+            Console.WriteLine("Суммирование вектора vector2 и вектора vector1: {0}", Vector.AddVector(vector1, vector2));
+            Console.WriteLine("Вычитание вектора vector1 и вектора vector2: {0}", Vector.SubtractVector(vector1, vector2));
+            Console.WriteLine("Умножение вектора vector2 и вектора vector1: {0}", Vector.MultiplicateVector(vector1, vector2));
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CourseTasks/Vector/Vector.cs
@@ -0,0 +1,212 @@
+﻿using System;
+
+namespace Vector
+{
+    class Vector
+    {
+        private double[] vector;
+
+        public Vector(int n)
+        {
+            if (n <= 0)
+            {
+                throw new ArgumentException("Размерность должна быть > 0");
+            }
+
+            this.vector = new double[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                this.vector[i] = 0.0;
+            }
+        }
+
+        public Vector(Vector vector)
+        {
+            this.vector = vector.vector;
+        }
+
+        public Vector(double[] vector)
+        {
+            this.vector = vector;
+        }
+
+        public Vector(int n, double[] vector)
+        {
+            if (n <= 0)
+            {
+                throw new ArgumentException("Размерность должна быть > 0");
+            }
+
+            this.vector = new double[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                if (i <= vector.Length - 1)
+                {
+                    this.vector[i] = vector[i];
+                }
+                else
+                {
+                    this.vector[i] = 0.0;
+                }
+            }
+        }
+
+        public int GetSize()
+        {
+            return vector.Length;
+        }
+
+        public override string ToString()
+        {
+            string stringVector = "{";
+
+            for (int i = 0; i < GetSize(); i++)
+            {
+                stringVector += GetVectorValue(i) + ", ";
+            }
+            stringVector = stringVector.Remove(stringVector.Length - 2, 2);
+            stringVector += "}";
+
+            return stringVector;
+        }
+
+        public double[] AddVector(Vector vector)
+        {
+            if (this.vector.Length < vector.GetSize())
+            {
+                Array.Resize(ref this.vector, vector.GetSize());
+            }
+
+            for (int i = 0; i < vector.vector.Length; i++)
+            {
+                this.vector[i] = this.vector[i] + vector.vector[i];
+            }
+
+            return this.vector;
+        }
+
+        public double[] SubtractVector(Vector vector)
+        {
+            if (this.vector.Length < vector.GetSize())
+            {
+                Array.Resize(ref this.vector, vector.GetSize());
+            }
+
+            for (int i = 0; i < vector.vector.Length; i++)
+            {
+                this.vector[i] = this.vector[i] - vector.vector[i];
+            }
+
+            return this.vector;
+        }
+
+        public double[] MultiplicateVector(double scalar)
+        {
+            for (int i = 0; i < this.vector.Length; i++)
+            {
+                this.vector[i] = this.vector[i] * scalar;
+            }
+
+            return this.vector;
+        }
+
+        public double[] ReverseVector()
+        {
+            for (int i = 0; i < this.vector.Length; i++)
+            {
+                this.vector[i] = this.vector[i] * (-1);
+            }
+
+            return this.vector;
+        }
+
+        public double GetLengthVector()
+        {
+            double lenghtVector = 0;
+
+            for (int i = 0; i < this.vector.Length; i++)
+            {
+                lenghtVector = lenghtVector + Math.Pow(this.vector[i], 2);
+            }
+
+            return Math.Sqrt(lenghtVector);
+        }
+
+        public void SetVectorValue(int index, double value)
+        {
+            this.vector[index] = value;
+        }
+
+        public double GetVectorValue(int index)
+        {
+            return this.vector[index];
+        }
+
+        public override bool Equals(object component)
+        {
+            if (ReferenceEquals(component, this))
+            {
+                return true;
+            }
+
+            if (ReferenceEquals(component, null) || component.GetType() != this.GetType())
+            {
+                return false;
+            }
+            Vector v = (Vector)component;
+
+            return this.vector == v.vector && this.vector.Length == v.vector.Length;
+        }
+
+        public override int GetHashCode()
+        {
+            int prime = 11;
+            int hash = 1;
+            for (int i = 0; i < this.vector.Length; i++)
+            hash = prime * hash + this.vector[i].GetHashCode();
+
+            return hash;
+        }
+
+        public static Vector AddVector(Vector vector1, Vector vector2)
+        {
+            Vector resultVector = new Vector(vector1.GetSize(), vector1.vector);
+            resultVector.AddVector(vector2);
+
+            return resultVector;
+        }
+
+        public static Vector SubtractVector(Vector vector1, Vector vector2)
+        {
+            Vector resultVector = new Vector(vector1.GetSize(), vector1.vector);
+            resultVector.SubtractVector(vector2);
+
+            return resultVector;
+        }
+
+        public static double MultiplicateVector(Vector vector1, Vector vector2)
+        {
+            int resultSize = 0;
+
+            if (vector1.GetSize() > vector2.GetSize())
+            {
+                resultSize = vector2.GetSize();
+            }
+            else
+            {
+                resultSize = vector1.GetSize();
+            }
+
+            double result = 0.0;
+
+            for (int i = 0; i < resultSize; i++)
+            {
+                result = result + vector1.vector[i] * vector2.vector[i];
+            }
+
+            return result;
+        }
+    }
+}
